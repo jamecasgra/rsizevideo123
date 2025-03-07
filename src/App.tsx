ea0f06e96f4c66ca6fd49f015ff908223ffba4e3
jamecasgra <128428242+jamecasgra@users.noreply.github.com>
2025-03-07 18:13:13 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import VideoCompressor from './components/VideoCompressor';
+import VideoDownload from './components/VideoDownload';
 import FAQ from './components/FAQ';
 import PrivacyPolicy from './components/PrivacyPolicy';
 import Terms from './components/Terms';
@@ -15,10 +16,11 @@ function App() {
         <main className="flex-grow">
           <Routes>
             <Route path="/" element={<VideoCompressor />} />
+            <Route path="/download/:id" element={<VideoDownload />} />
             <Route path="/faq" element={<FAQ />} />
             <Route path="/privacy" element={<PrivacyPolicy />} />
             <Route path="/terms" element={<Terms />} />
-            <Route path="/download/:folderId/:filename" element={<VideoCompressor />} />
+            <Route path="*" element={<Navigate to="/" replace />} />
           </Routes>
         </main>
         <Footer />
